01af45519aeb3d902fb6b3e58ae6e8103a0f58a2
williambpereira21 <william.pbarbosagon@gmail.com>
2025-09-23 18:38:35 -0300

    alteração dia 23/09/2025

--- aula04.py
+++
@@ -1,8 +0,0 @@
-#demonstração do uso de if 
-print('digite sua idade:')
-idade= int(input())
-
-if idade<18:
-    print('você não é maior de idade!')
-    print('não poderá realizar operações bancárias!')
-    print('obrigado por escolher os nossos serviços!')
